dcd870e5c005bdd599083bb43cc519ad1b4740a8
yummyjackalmeat <dallinpeacock@gmail.com>
2025-09-19 10:51:17 -0500

    adding 9-17-25

---
+++ September-2025/fcc-9-17-25.js
@@ -0,0 +1,27 @@
+/*
+Given a string, return a URL-friendly version of the string using the following constraints:
+All letters should be lowercase.
+All characters that are not letters, numbers, or spaces should be removed.
+All spaces should be replaced with the URL-encoded space code %20.
+Consecutive spaces should be replaced with a single %20.
+The returned string should not have leading or trailing %20.
+ */
+
+function generateSlug(str) {
+    let changedStr = str.replace(/[^a-zA-Z0-9 ]/g, '').trim().toLowerCase().replace(/ +/g, '%20');
+  return strchangedStr;
+}
+
+generateSlug("helloWorld")
+generateSlug("hello world!")
+generateSlug(" hello-world ")
+generateSlug("hello  world")
+generateSlug("  ?H^3-1*1]0! W[0%R#1]D  ")
+
+/* 
+generateSlug("helloWorld") should return "helloworld".
+generateSlug("hello world!") should return "hello%20world".
+generateSlug(" hello-world ") should return "helloworld".
+generateSlug("hello  world") should return "hello%20world".
+generateSlug("  ?H^3-1*1]0! W[0%R#1]D  ") should return "h3110%20w0r1d".
+*/
--- September-2025/fcc-9-19-25.js
+++ September-2025/fcc-9-19-25.js
@@ -16,13 +16,13 @@ function numberOfPhotos(photoSizeMb, hardDriveSizeGb) {
   return calculateNumOfPhotos();
 }
 
-/*
+/* 
 console.log(numberOfPhotos(1, 1));
 console.log(numberOfPhotos(2, 1));
 console.log(numberOfPhotos(4, 256));
 console.log(numberOfPhotos(3.5, 750));
 console.log(numberOfPhotos(3.5, 5.5)); 
-*/
+ */
 
 /*
 1. numberOfPhotos(1, 1) should return 1000.

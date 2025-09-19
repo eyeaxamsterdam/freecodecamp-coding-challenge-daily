a5486bb12b2b73924491d388ca9d9c2a361c633f
yummyjackalmeat <dallinpeacock@gmail.com>
2025-09-19 09:17:09 -0500

    changed to js file

--- September-2025/fcc-9-19-25.js
+++ September-2025/fcc-9-19-25.js
@@ -1,9 +1,10 @@
-** start of script.js **
 /*
+
 Given a photo size in megabytes (MB), and hard drive capacity in gigabytes (GB), return the number of photos the hard drive can store using the following constraints:
 
 1 gigabyte equals 1000 megabytes.
 Return the number of whole photos the drive can store.
+
 */
 
 
@@ -11,9 +12,17 @@ function numberOfPhotos(photoSizeMb, hardDriveSizeGb) {
 	// reusable conversion function
     let convertToMb = (mb) => mb*1000;
 	// use .floor to round down
-    let calculateNumOfPhotos = () => Math.floor(convertToMb(hardDriveSizeGb)/photoSizeMb)
+    let calculateNumOfPhotos = () => Math.floor(convertToMb(hardDriveSizeGb)/photoSizeMb);
   return calculateNumOfPhotos();
 }
+
+/*
+console.log(numberOfPhotos(1, 1));
+console.log(numberOfPhotos(2, 1));
+console.log(numberOfPhotos(4, 256));
+console.log(numberOfPhotos(3.5, 750));
+console.log(numberOfPhotos(3.5, 5.5)); 
+*/
 
 /*
 1. numberOfPhotos(1, 1) should return 1000.
@@ -22,6 +31,3 @@ function numberOfPhotos(photoSizeMb, hardDriveSizeGb) {
 4. numberOfPhotos(3.5, 750) should return 214285.
 5. numberOfPhotos(3.5, 5.5) should return 1571.
 */
-
-** end of script.js **
-

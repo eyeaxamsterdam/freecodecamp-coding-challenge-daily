7f3c10fd4f298da75c005931245fbe22d9bba5f4
yummyjackalmeat <dallinpeacock@gmail.com>
2025-10-07 14:57:02 -0500

    10-7-25 started

---
+++ 10-25/10-07-25.js
@@ -0,0 +1,53 @@
+/*
+Space Week Day 4: Landing Spot
+In day four of Space Week, you are given a matrix of numbers (an array of arrays), representing potential landing spots for your rover. Find the safest landing spot based on the following rules:
+
+Each spot in the matrix will contain a number from 0-9, inclusive.
+Any 0 represents a potential landing spot.
+Any number other than 0 is too dangerous to land. The higher the number, the more dangerous.
+The safest spot is defined as the 0 cell whose surrounding cells (up to 4 neighbors, ignore diagonals) have the lowest total danger.
+Ignore out-of-bounds neighbors (corners and edges just have fewer neighbors).
+Return the indices of the safest landing spot. There will always only be one safest spot.
+For instance, given:
+
+[
+  [1, 0],
+  [2, 0]
+]
+Return [0, 1], the indices for the 0 in the first array.
+*/
+
+function findLandingSpot(matrix) {
+    let location = [];
+    let total = 0;
+    let tempTotal = 0
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            let [left, right, up, down] = 0;
+            if (j !== 0) {
+                left = matrix[i][j - 1] ? matrix[i][j - 1] : 0;
+                console.log('left: ',left, ' ',matrix[i][j], matrix[i]);
+            }
+            if (i !==0) {
+                up = matrix[i - 1][j] ? matrix[i - 1][j] : 0;
+                console.log('up: ', up, matrix[i][j], matrix[i]);
+            }
+            /* let left = matrix[i][j - 1] ? matrix[i][j - 1] : 0 
+            let right = matrix[i][j + 1] ? matrix[i][j + 1] : 0
+            let up = matrix[i - 1][j] ?  matrix[i-1][j] : 0
+            let down = matrix[i + 1][j] ? matrix[i +1][j] : 0
+
+            tempTotal = left + right + up + down;
+            console.log(tempTotal); */
+        }
+    }
+    
+   
+
+    return location;
+}
+
+findLandingSpot([[1, 0], [2, 0]]); //should return [0, 1].
+/* findLandingSpot([[9, 0, 3], [7, 0, 4], [8, 0, 5]]) //should return [1, 1].
+findLandingSpot([[1, 2, 1], [0, 0, 2], [3, 0, 0]]) //should return [2, 2].
+findLandingSpot([[9, 6, 0, 8], [7, 1, 1, 0], [3, 0, 3, 9], [8, 6, 0, 9]]) //should return [2, 1]. */
